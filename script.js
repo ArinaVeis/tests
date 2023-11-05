a1e351dd96d285effb80ed05c2a9ba23920f489f
ArinaVeis <118526878+ArinaVeis@users.noreply.github.com>
2023-11-05 18:30:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let popupBg = document.querySelector('.second-win-bg');
+let popup = document.querySelector('.second-win');
+let openPopupButtons = document.querySelectorAll('.open-second-win');
+
+
+openPopupButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        popupBg.classList.add('active');
+        popup.classList.add('active');
+    })
+});
+
+document.addEventListener('click', (e) => {
+    if(e.target === popupBg) {
+        popupBg.classList.remove('active');
+        popup.classList.remove('active');
+    }
+});
